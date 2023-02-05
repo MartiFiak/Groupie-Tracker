17a9dbea52d6db3e2b84e7874214b82f15956872
mleo <leo.mace@ynov.com>
2023-02-05 12:40:24 +0100

    All Artist [Front Update]

--- server/js/allartist.js
+++ server/js/allartist.js
@@ -1,3 +1,7 @@
+let filter = document.querySelector(".filter");
+
+var filterVisibility = false;
+
 function changesize(self){
     self.style.width = "50vh";
     self.querySelector("img").style.filter = "saturate(1)"
@@ -7,3 +11,20 @@ function mouseleave(self){
     self.style.width = "15vh";
     self.querySelector("img").style.filter = "saturate(20%)"
 }
+
+function _showFilterAA(){
+    if (filterVisibility){
+        filter.style.height = "0px";
+        filterVisibility = false;
+    } else {
+        filter.style.height = "auto";
+        filterVisibility = true;
+    }
+}
+
+const scrollContainer = document.querySelector(".sect2AA");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
